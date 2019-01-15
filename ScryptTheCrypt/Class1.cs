a8a6fe42bb04f1a63b4cfa013cb3c252f7d2f512
Kai Arnold <cdn.kai@gmail.com>
2019-01-14 21:02:18 -0800

    initial commit, with unit tests

--- ScryptTheCrypt/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ScryptTheCrypt
-{
-    public class Class1
-    {
-    }
-}
---
+++ ScryptTheCrypt/GameActor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ScryptTheCrypt
+{
+    public sealed class GameActor
+    {
+        public float baseHealth { get; }
+        public float health { get; private set; }
+        public bool alive
+        {
+            get
+            {
+                return health > 0;
+            }
+        }
+        public GameActor(float baseHealth)
+        {
+            if (baseHealth <= 0)
+            {
+                throw new ArgumentOutOfRangeException("baseHealth", "baseHealth must be greater than zero");
+            }
+            this.baseHealth = baseHealth;
+        }
+        public void takeDamage(float d)
+        {
+            health = Math.Max(0, health - d);
+        }
+        public void heal(float h)
+        {
+            health = Math.Min(baseHealth, health + h);
+        }
+    }
+}
